38c1d0d23402580630830967dd2e0ee3df3bb9e5
Rushali GUPTA <rushali.gupta@amadeus.com>
2022-04-13 00:12:34 +0530

    Name Calculator

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 (function (){
     'use strict';
 
+
+    
     /*
-    in angular object, module method expects first parameter as our app name,
-    second parameter is aray of dependencies,
-    in this case we don't have any dependencies so pass an empty array
-    */
+    First app
+    // in angular object, module method expects first parameter as our app name,
+    // second parameter is aray of dependencies,
+    // in this case we don't have any dependencies so pass an empty array
+    
     angular.module('myFirstApp', [])
     //module function returns module instance.
     .controller("myFirstController", function($scope){
         $scope.name="Rushali";  
     }); 
-    /*
-    the way we define ViewModel of view,
-    takes Controller name and function that defines functionality for the controller as second parameter.
+    
+    // the way we define ViewModel of view,
+    // takes Controller name and function that defines functionality for the controller as second parameter.
+    
     */
+
+    angular.module("nameCalculator",[])
+    .controller("nameCalculatorController", function ($scope){
+        $scope.fullName= "";
+        $scope.totalNameValue=0;
+
+        $scope.displayNumeric = function(){
+            var totalValue = calculateNumericForString($scope.fullName); 
+            $scope.totalNameValue= totalValue;
+        };
+
+        function calculateNumericForString(string){
+            var totalStringValue = 0;
+            for(let i=0;i<string.length;i++){
+                totalStringValue += string.charCodeAt(i);
+            }
+            return totalStringValue;
+        }
+    });
+
 })();
