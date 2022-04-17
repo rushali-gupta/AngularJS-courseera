b7ca397d793197e33541b7c2d6adf88e2471c71e
Rushali GUPTA <rushali.gupta@amadeus.com>
2022-04-17 18:41:24 +0530

    Custom Services (.service)

--- Module-2/app1.js
+++ Module-2/app1.js
@@ -1,10 +1,10 @@
 (function(){
     'use strict';
-    angular.module('ControllerAsApp',[])
-    // .controller('ParentController1',ParentController1)
-    // .controller('ChildController1',ChildController1)
-    .controller('ParentController2',ParentController2)
-    .controller('ChildController2',ChildController2);
+    // angular.module('ControllerAsApp',[])
+    // // .controller('ParentController1',ParentController1)
+    // // .controller('ChildController1',ChildController1)
+    // .controller('ParentController2',ParentController2)
+    // .controller('ChildController2',ChildController2);
 
     // ParentController1.$inject = ['$scope'];
     // function ParentController1($scope){
@@ -33,16 +33,60 @@
 
 
 
-    function ParentController2(){
-        let parent = this;
-        parent.value = 1;
+    // function ParentController2(){
+    //     let parent = this;
+    //     parent.value = 1;
+    // }
+
+    // ChildController2.inject = ['$scope'];
+    // function ChildController2($scope){
+    //     let child = this;
+    //     child.value = 5;
+    //     console.log('ChildController2 $scope: ',$scope);
+    // }
+
+    angular.module('CustomServicesApp',[])
+    .controller('AddItemController',AddItemController)
+    .controller('ShowItemController',ShowItemController)
+    .service('ItemsListService',ItemsListService);
+
+    AddItemController.$inject = ['ItemsListService'];
+    function AddItemController(ItemsListService){
+        let ItemAdder=this;
+        ItemAdder.itemName="";
+        ItemAdder.itemQuantity="";
+        ItemAdder.addItem = function(){
+            ItemsListService.addItem(ItemAdder.itemName,ItemAdder.itemQuantity);
+            ItemAdder.itemName="";
+            ItemAdder.itemQuantity="";
+        };
     }
 
-    ChildController2.inject = ['$scope'];
-    function ChildController2($scope){
-        let child = this;
-        child.value = 5;
-        console.log('ChildController2 $scope: ',$scope);
+    ShowItemController.$inject = ['ItemsListService'];
+    function ShowItemController(ItemsListService){
+        let ShowItem=this;
+        ShowItem.list = ItemsListService.getItems(); 
+        ShowItem.removeItem = function(index){
+            ItemsListService.removeItem(index);
+        }
+    }
+
+    function ItemsListService(){
+        let service = this;
+        let items=[];
+        service.addItem = function(itemName,itemQuantity){
+            let newItem={
+                name: itemName,
+                quantity: itemQuantity
+            };
+            items.push(newItem);
+        };
+        service.getItems = function() {
+            return items;
+        };
+        service.removeItem = function(index){
+            items.splice(index,1);
+        };
     }
 
 })();
